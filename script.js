893d9fd73c62888c5fef341c92f0ddd150c188a7
MariyahW <mariyah.watson@gmail.com>
2026-07-12 16:19:15 -0400

    added node.js and built out script more/added some api work

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-const dropdown = document.getElementById("citySelect")
+const axios = require("axios");
+
+const dropdown = document.getElementById("citySelect");
+
+async function forecast(lat, long) {
+  const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch`;
+
+  const response = await axios.get(url);
+  console.log(response.data);
+}
